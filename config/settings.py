b65d4bbd453dd0f38fe322214ee72756fcea7cc3
root <ballbes@mail.ru>
2023-02-27 16:02:54 +0300

    Lesson_5 Finish

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+gitINSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
